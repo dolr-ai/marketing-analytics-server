4be51dff7a20761535cbe8f97e31d16b0c24908c
Prakash Bhatt <prk-jr@Prakashs-MacBook-Air.local>
2025-06-17 14:59:42 +0530

    Added ip to location for bigquery only

---
+++ offchain-server/src/adapters/loc_from_ip.rs
@@ -0,0 +1,60 @@
+use std::env;
+
+use reqwest;
+use serde::Deserialize;
+use serde_json::Value;
+
+use crate::domain::errors::AppError;
+
+#[derive(Deserialize, Debug)]
+pub struct IpInfo {
+    city: Option<String>,
+    region: Option<String>,
+    country: Option<String>,
+    loc: Option<String>,
+    postal: Option<String>,
+    timezone: Option<String>,
+}
+
+pub async fn insert_ip_details(payload: &mut Value) -> Result<(), AppError> {
+    let ip_token = env::var("IP_TOKEN").ok();
+    let ip = payload
+        .get("ip_addr")
+        .and_then(|f| f.as_str())
+        .map(str::to_owned);
+    if let Some(ip) = ip {
+        let url = format!("https://ipinfo.io/{}/json", ip);
+
+        let url = if let Some(token) = ip_token {
+            format!("{}?token={}", url, token)
+        } else {
+            url
+        };
+
+        let response = reqwest::get(&url).await?;
+        let ip_info: IpInfo = response.json().await?;
+        if let Some(city) = ip_info.city {
+            payload["city"] = city.into();
+        }
+        if let Some(region) = ip_info.region {
+            payload["region"] = region.into();
+        }
+        if let Some(country) = ip_info.country {
+            payload["country"] = country.into();
+        }
+        if let Some(loc) = ip_info.loc {
+            let coords: Vec<&str> = loc.split(',').collect();
+            if coords.len() == 2 {
+                payload["latitude"] = coords[0].into();
+                payload["longitude"] = coords[1].into();
+            }
+        }
+        if let Some(postal) = ip_info.postal {
+            payload["postal"] = postal.into();
+        }
+        if let Some(timezone) = ip_info.timezone {
+            payload["timezone"] = timezone.into();
+        }
+    }
+    Ok(())
+}
